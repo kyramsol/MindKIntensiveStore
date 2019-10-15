6fa864d6abbb9baff404a352a342219f7df36eb8
seko <hovaysa.v.gito@gmail.com>
2019-10-15 18:44:36 +0300

    feat(factories): factories updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './HeaderComponent'
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./HeaderComponent";
 
-import Footer from './FooterComponent'
+import Footer from "./FooterComponent";
 
-import Content from "./ContentComponent"
+import Content from "./ContentComponent";
 
-
-
-
-    class Page extends Component {
-    render()
-    {
-        return (
-            <div>
-                <Header/>
-                <Content/>
-                <Footer/>
-
-            </div>
-
-            );
-    }
-
-
+class Page extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
 }
 export default Page;
